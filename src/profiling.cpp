3ec32a5f4f771f63b7ec732a93b5931f87e5ebbf
David M. Lary <857742+dmlary@users.noreply.github.com>
2025-01-25 10:02:58 -0600

    Only intialize profiling on apple devices
    fixes #1

--- src/profiling.cpp
+++ src/profiling.cpp
@@ -1,4 +1,8 @@
 #include "profiling.h"
 
+#ifdef __APPLE__
+
 os_log_t profiling_os_log = os_log_create("org.godot.gdextension.hexmap",
         OS_LOG_CATEGORY_POINTS_OF_INTEREST);
+
+#endif // __APPLE__
